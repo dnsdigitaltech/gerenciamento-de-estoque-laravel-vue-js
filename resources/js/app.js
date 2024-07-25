5b67dab27117e5660c44a69a66716c5af47b5cc3
Davi Bernardo <davi.bernardo@sbahq.org>
2024-07-25 18:48:15 -0300

    Configurando o layout da tela inicial do sistema

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,11 @@
 require('./bootstrap');
 
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-Vue.component('main-app', require('./components/mainApp.vue').default)
+Vue.use(VueRouter)
+
+Vue.component('app', require('./components/app.vue').default)
 
 const app = new Vue({
     el: '#app'
