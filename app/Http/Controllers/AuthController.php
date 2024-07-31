854445de26cfb88ac2ba0374f576e21015ec9188
Davi Bernardo <dnssites@gmail.com>
2024-07-31 20:17:36 -0300

    Criando a Helper Class (token, user)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -96,7 +96,10 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => Auth::factory()->getTTL() * 60
+            'expires_in' => Auth::factory()->getTTL() * 60,
+            'name' => Auth::user()->name,
+            'user_id' => Auth::user()->id,
+            'email' => Auth::user()->email,
         ]);
     }
 }
