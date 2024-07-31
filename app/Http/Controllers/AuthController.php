e2ee0faaa5600c6682b45f4b7eb7647c016519fa
Davi Bernardo <dnssites@gmail.com>
2024-07-31 19:36:38 -0300

    Criando um middleware JWT para autenticação

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,9 +18,9 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login','register']]);
+        $this->middleware('JWT', ['except' => ['login','register']]);
     }
-
+ 
     /**
      * Get a JWT via given credentials.
      *
